414ced12448c425dd49a946c5eeb6e60e1eb7cf4
redaalch <s133260695@taalim.ma>
2026-05-12 12:13:30 +0100

    Add .gitignore and implement basic Express server with WebSocket support

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const http = require('http');
-http.createServer(function (req,res) {
-	res.write("On the way to being a full snack engineer!");
-	res.end();
-}).listen(3000);
+const http = require("http");
+http
+  .createServer(function (req, res) {
+    res.write("On the way to being a full snack engineer!");
+    res.end();
+  })
+  .listen(3000);
 console.log("server working! On port 3000");
